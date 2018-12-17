0539ac6fd19234269d708a324962ad39b0c5d3a3
Preyesh Malde <bushido.consulting@hotmail.com>
2018-12-17 12:20:50 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import plotly from 'plotly.js/dist/plotly';
+import MyPlotlyEditor from './MyPlotlyEditor';
+import 'react-chart-editor/lib/react-chart-editor.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [
+        {
+          x: [1, 2, 3],
+          y: [2, 6, 3],
+          type: 'scatter',
+          mode: 'lines+points',
+          marker: {color: 'red'},
+        },
+        {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+      ],
+      layout: {width: 640, height: 480, title: 'A Fancy Plot'},
+      config: {editable: true}
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <MyPlotlyEditor
+          data={this.state.data}
+          layout={this.state.layout}
+          config={this.state.config}
+          plotly={plotly}
+        />
       </div>
     );
   }
